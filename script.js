cba0e7f162d1721ace3c06e88dc229627bcdefd9
InesRocketDigital <inescastelltort@gmail.com>
2025-04-21 13:02:44 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('setPlayers').addEventListener('click', function() {
+  const numPlayers = parseInt(document.getElementById('numPlayers').value);
+  const playerInputs = document.getElementById('playerInputs');
+  playerInputs.innerHTML = '';
+
+  for (let i = 1; i <= numPlayers; i++) {
+      playerInputs.innerHTML += `<input type="text" placeholder="Nombre del Jugador ${i}" class="playerName">`;
+  }
+
+  document.getElementById('playerNamesContainer').classList.remove('hidden');
+});
+
+document.getElementById('startGame').addEventListener('click', function() {
+  const playerNames = Array.from(document.querySelectorAll('.playerName')).map(input => input.value);
+  const totalPoints = Array(playerNames.length).fill(0);
+  const bazaHistory = [];
+
+  document.getElementById('playerNamesContainer').classList.add('hidden');
+  document.getElementById('gameContainer').classList.remove('hidden');
+
+  const totalPointsContainer = document.getElementById('totalPointsContainer');
+  totalPointsContainer.innerHTML = `<h3>Puntos Totales</h3>${playerNames.map((name, index) => `<p>${name}: <span id="points-${index}">0</span></p>`).join('')}`;
+
+  document.getElementById('addB
